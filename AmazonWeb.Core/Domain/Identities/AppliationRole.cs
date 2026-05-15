8d2b0ce172603381a39888003c00377821145b74
bunny <banhibikashsaha@gmail.com>
2026-05-15 12:35:22 +0530

    Created ApplicationRole

---
+++ AmazonWeb.Core/Domain/Identities/AppliationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmazonWeb.Core.Domain.Identities
+{
+    public class AppliationRole:IdentityRole<Guid>
+    {
+    }
+}
--- AmazonWeb.Core/obj/Debug/net10.0/AmazonWeb.Core.AssemblyInfo.cs
+++ AmazonWeb.Core/obj/Debug/net10.0/AmazonWeb.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazonWeb.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c806c5d9569ed6ba8638743c09549b62a32482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d32d91ab73dc5b232c50d85653eddc279f1a8a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazonWeb.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazonWeb.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
