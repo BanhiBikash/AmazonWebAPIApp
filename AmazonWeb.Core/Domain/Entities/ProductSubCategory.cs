ac236c9b75855345839d5ec3872e1328641b69b7
bunny <banhibikashsaha@gmail.com>
2026-05-15 12:38:55 +0530

    Added sub-category

--- AmazonWeb.Core/Domain/Entities/ProductSubCategory.cs
+++ AmazonWeb.Core/Domain/Entities/ProductSubCategory.cs
@@ -1,18 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AmazonWeb.Core.Domain.Entities
+﻿namespace AmazonWeb.Core.Domain.Enums
 {
     public enum ProductSubCategory
     {
-        Electronics_Mobiles,
-        Electronics_Laptops,
-        Electronics_Cameras,
+        // Mobiles
+        Mobile_Smartphones,
+        Mobile_FeaturePhones,
+        Mobile_Accessories,
+        Mobile_Tablets,
+
+        // Laptops
+        Laptop_Gaming,
+        Laptop_Business,
+        Laptop_Ultrabooks,
+        Laptop_Accessories,
+
+        // Fashion
         Fashion_Men,
         Fashion_Women,
         Fashion_Kids,
-        Books_Fiction,
-        Books_NonFiction
+        Fashion_Accessories,
+
+        // Books
+        Book_Fiction,
+        Book_NonFiction,
+        Book_Educational,
+        Book_Comics,
+
+        // Home Appliances
+        HomeAppliance_Kitchen,
+        HomeAppliance_Laundry,
+        HomeAppliance_Cleaning,
+        HomeAppliance_ClimateControl,
+
+        // Furniture
+        Furniture_LivingRoom,
+        Furniture_Bedroom,
+        Furniture_Office,
+        Furniture_Outdoor,
+
+        // Toys
+        Toy_ActionFigures,
+        Toy_Dolls,
+        Toy_Educational,
+        Toy_Puzzles,
+
+        // Sports
+        Sports_Indoor,
+        Sports_Outdoor,
+        Sports_Fitness,
+        Sports_Accessories,
+
+        // Beauty
+        Beauty_Skincare,
+        Beauty_Haircare,
+        Beauty_Makeup,
+        Beauty_Fragrance,
+
+        // Health
+        Health_Supplements,
+        Health_MedicalDevices,
+        Health_PersonalCare,
+        Health_Fitness,
+
+        // Groceries
+        Grocery_Fruits,
+        Grocery_Vegetables,
+        Grocery_Beverages,
+        Grocery_Snacks,
+
+        // Pets
+        Pet_Food,
+        Pet_Toys,
+        Pet_Grooming,
+        Pet_Accessories,
+
+        // Automotive
+        Automotive_CarAccessories,
+        Automotive_BikeAccessories,
+        Automotive_SpareParts,
+        Automotive_Tools,
+
+        // Jewelry
+        Jewelry_Necklaces,
+        Jewelry_Rings,
+        Jewelry_Bracelets,
+        Jewelry_Earrings,
+
+        // Shoes
+        Shoe_SportsShoes,
+        Shoe_CasualShoes,
+        Shoe_FormalShoes,
+        Shoe_Sandals,
+
+        // Stationary
+        Stationary_Pens,
+        Stationary_Notebooks,
+        Stationary_ArtSupplies,
+        Stationary_OfficeSupplies
     }
 }
