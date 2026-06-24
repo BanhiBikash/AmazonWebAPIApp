1e6405b508266b543640db1b34ba5423b004bb94
bunny <banhibikashsaha@gmail.com>
2026-06-25 00:26:01 +0530

    created constructor and mocked product repo: writing tests next

--- AmazonWeb.Core.UnitTests/ProductServiceUnitTests.cs
+++ AmazonWeb.Core.UnitTests/ProductServiceUnitTests.cs
@@ -1,15 +1,29 @@
-﻿using AmazonWeb.Core.ServiceContracts.ProductContracts;
+﻿using AmazonWeb.Core.Domain.RepositoryContract;
+using AmazonWeb.Core.ServiceContracts;
+using AmazonWeb.Core.ServiceContracts.ProductContracts;
+using AmazonWeb.Core.Services;
+using Castle.Core.Configuration;
+using Moq;
 
 namespace AmazonWeb.Core.UnitTests
 {
     public class ProductServiceUnitTests
     {
         private readonly IProductService _productService; 
-        
-        public ProductServiceUnitTests(IProductService productService)
+        private readonly IProductRepository _productRepository;
+        private readonly Mock<IProductRepository> _mockProductRepository;
+        private readonly IFileService _fileService;
+        private readonly IConfiguration _configuration;
+
+        public ProductServiceUnitTests(IFileService fileService, IConfiguration configuration)
         {
-            // Initialize the product service with a mock or real implementation
-            _productService = productService;
+            //injecting necessary dependencies into the constructor
+            _fileService = fileService;
+            _configuration = configuration;
+
+            _mockProductRepository = new Mock<IProductRepository>();    //created mock repository of Product type
+            _productRepository = _mockProductRepository.Object; //assigned mock obj to product repository
+            _productService = new ProductService(_productRepository, _fileService, _configuration); //created product service by passing mocked product repository
         }
     }
 }
